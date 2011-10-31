62c49963ec93786ce345d85fbd1e3baf7a62cd59
Matt Colthurst <Matt@Matt-Laptop.(none)>
2011-10-31 12:55:47 -0400

    First Commit

---
+++ src/com/cs/uwindsor/group/eLecture/ELectureActivity.java
@@ -0,0 +1,13 @@
+package com.cs.uwindsor.group.eLecture;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ELectureActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
