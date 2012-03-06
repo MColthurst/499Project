1ef75d051097ae29b0c30905313b370674447d5e
Matt Colthurst <Matt@Matt-Laptop.(none)>
2012-03-06 18:25:24 -0500

    Fixed Search page now can search on any of 4 feilds results still sent to log. Working on the List page

--- src/com/cs/uwindsor/group/CARE/SearchActivity.java
+++ src/com/cs/uwindsor/group/CARE/SearchActivity.java
@@ -3,6 +3,7 @@ package com.cs.uwindsor.group.CARE;
 import java.util.Map;
 import java.util.TreeMap;
 
+import com.cs.uwindsor.group.CARE.TypeActivity.MyOnItemSelectedListener;
 import com.cs.uwindsor.group.CARE.utils.SearchUtils;
 
 import android.app.Activity;
@@ -11,20 +12,36 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class SearchActivity extends Activity{
 	Button button;
+	TextView id;
 	TextView name;
-	
+	TextView price;
+	String rating = new String();
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.search);
 	    
+	    Spinner Spinner = (Spinner) findViewById(R.id.ratingMenu);
+	    ArrayAdapter<CharSequence> Adapter = ArrayAdapter.createFromResource(
+	            this, R.array.rating_array, android.R.layout.simple_spinner_item);
+	    Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    Spinner.setAdapter(Adapter);
+	    
 	    name = (TextView)findViewById(R.id.nameText);
+	    id = (TextView)findViewById(R.id.idText);
+	    price = (TextView)findViewById(R.id.priceText);
+        Spinner.setOnItemSelectedListener(new MyOnItemSelectedListener());
         button = (Button)findViewById(R.id.s_button);
         button.setOnClickListener(sListener);     
 	}
@@ -37,11 +54,28 @@ public class SearchActivity extends Activity{
         	i.putExtra("xml", xml);
         	startActivity(i);
         }
-
-		private Map<String, String> buildmap() {
-        	Map<String, String> map = new TreeMap<String, String>();
-			map.put("name", name.getText().toString());
-			return map;
-		}
     };
+
+    private class MyOnItemSelectedListener implements OnItemSelectedListener {
+
+    	    public void onItemSelected(AdapterView<?> parent,
+    	        View view, int pos, long id) {
+    	    		rating = parent.getItemAtPosition(pos).toString();
+    	    }
+        
+    	    @Override
+    	    public void onNothingSelected(AdapterView<?> arg0) {
+    	    	// TODO Auto-generated method stub
+		
+    	    }
+    }
+    
+	private Map<String, String> buildmap() {
+    	Map<String, String> map = new TreeMap<String, String>();
+		map.put("name", name.getText().toString());
+		map.put("id", id.getText().toString());
+		map.put("price", price.getText().toString());
+		map.put("rating", rating);
+		return map;
+	}
 }
