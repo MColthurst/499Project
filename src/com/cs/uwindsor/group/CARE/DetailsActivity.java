cc8bcacd748ad4b5f168a1443bac136d1ba48899
Matt Colthurst <Matt@Matt-Laptop.(none)>
2012-03-22 13:24:30 -0400

    Images working, the size is a slight issue some look ok others are pixelated. Also fixed a few minor bugs. There is still an issue when writing a review the stars on the details page are not updating but everything else is so I don't know what is causing that.

--- src/com/cs/uwindsor/group/CARE/DetailsActivity.java
+++ src/com/cs/uwindsor/group/CARE/DetailsActivity.java
@@ -1,10 +1,6 @@
 package com.cs.uwindsor.group.CARE;
 
-import com.cs.uwindsor.group.CARE.utils.ReviewUtils;
-
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -12,13 +8,18 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.cs.uwindsor.group.CARE.adapters.ImageLoader;
+import com.cs.uwindsor.group.CARE.utils.ReviewUtils;
+
 public class DetailsActivity extends Activity{
 	Activity a = new Activity();
+	String imgURL = "http://care.cs.uwindsor.ca/imgs/";
+	public ImageLoader imageLoader;
 	
-	TextView ratingtop;
 	TextView brand;
 	TextView name;
 	TextView price;
@@ -28,6 +29,7 @@ public class DetailsActivity extends Activity{
 	TextView reviewName;
 	TextView reviewRating;
 	TextView reviewComment;
+	ImageView picture;
 	RatingBar rating;
 	Button rate;
 	Button reviews;
@@ -38,7 +40,7 @@ public class DetailsActivity extends Activity{
 	    
 	    a = this;
 	    
-	    ratingtop = (TextView) findViewById(R.id.samplereview);
+	    picture = (ImageView) findViewById(R.id.picture);
 	    name = (TextView) findViewById(R.id.name);
 	    brand = (TextView) findViewById(R.id.brand);
 	    price = (TextView) findViewById(R.id.price);
@@ -57,7 +59,7 @@ public class DetailsActivity extends Activity{
 	    desc.setText("Product Description: \n" + getIntent().getStringExtra("desc"));
 	    desc.setMovementMethod(new ScrollingMovementMethod());
 	    brand.setText("Manufacturer: "+getIntent().getStringExtra("make"));
-	    numRatings.setText("("+getIntent().getStringExtra("ratingcount")+" Ratings)");
+	    numRatings.setText("("+getIntent().getStringExtra("ratingcount")+" reviews)");
 	    
 	    
 	    switch (Integer.parseInt(getIntent().getStringExtra("type"))){
@@ -72,16 +74,22 @@ public class DetailsActivity extends Activity{
 	    
 	    if(getIntent().hasExtra("newReview")){
 	    	String[] review = getIntent().getStringArrayExtra("newReview");
-	    	reviewRating.setText("Rating: "+review[0]+"/5");
+	    	reviewRating.setText("Rating: "+review[0]+"/5 Stars");
 	    	reviewName.setText("By: "+review[1]);
 	    	reviewComment.setText(review[2]);
 	    }
 	    else{
 	    	String[] review = ReviewUtils.firstReview(getIntent().getStringArrayListExtra("reviews"));
 	    	if(review!=null){
-	    		reviewRating.setText("Rating: "+review[0]+"/5");
-	    		reviewName.setText("By: "+review[1]);
-	    		reviewComment.setText(review[2]);
+	    		reviewRating.setText("Rating: "+review[0]+"/5 Stars");
+	    		if(review[1].equals(""))
+	    			reviewName.setText("By: Anonymous");
+	    		else
+	    			reviewName.setText("By: "+review[1]);
+	    		if(review[2].equals(""))
+	    			reviewComment.setText("No Comment");
+	    		else
+	    			reviewComment.setText(review[2]);
 	    	}
 	    	else{
 	    		reviewRating.setText("");
@@ -91,9 +99,20 @@ public class DetailsActivity extends Activity{
 	    		
 	    }
 	    	
-
-	    rating.setRating(Float.parseFloat(getIntent().getStringExtra("rating")));
-	    rating.setIsIndicator(true);
+	    if(getIntent().hasCategory("newRating")){
+	    	Log.d("newRating", String.valueOf(getIntent().getFloatExtra("newRating", 0)));
+	    	rating.setIsIndicator(false);
+	    	rating.setRating(getIntent().getFloatExtra("newRating", 0));
+	    	rating.setIsIndicator(true);
+	    }
+	    else{
+	    	rating.setRating(Float.parseFloat(getIntent().getStringExtra("rating")));
+	    	rating.setIsIndicator(true);
+	    }
+	    
+		imageLoader = new ImageLoader(this.getApplicationContext());
+		ImageView image = (ImageView) findViewById(R.id.picture);
+		imageLoader.DisplayImage(imgURL+getIntent().getStringExtra("img"), a, image);
 	    
 	    rate.setOnClickListener(sListener); 
 	    reviews.setOnClickListener(rListener); 
@@ -104,22 +123,24 @@ public class DetailsActivity extends Activity{
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 		
-		int num = Integer.parseInt(a.getIntent().getStringExtra("ratingcount"));
-		num++;
+		if(resultCode == RESULT_OK){
+			
+			int num = Integer.parseInt(a.getIntent().getStringExtra("ratingcount"));
+			num++;
 		
-		float sum = Float.parseFloat(a.getIntent().getStringExtra("ratingsum"));
-		sum = sum + Float.parseFloat(data.getStringArrayExtra("review")[0]);
+			float sum = Float.parseFloat(a.getIntent().getStringExtra("ratingsum"));
+			sum = sum + Float.parseFloat(data.getStringArrayExtra("review")[0]);
 		
-		Log.d("avg", String.valueOf(sum/num));
+			Log.d("avg", String.valueOf(sum/num));
 		
-		a.getIntent().removeExtra("rating");
-		a.getIntent().putExtra("rating", String.valueOf(sum/num));
+			a.getIntent().putExtra("newRating", (sum/num));
 		
-		a.getIntent().removeExtra("ratingcount");
-		a.getIntent().putExtra("ratingcount", String.valueOf(num));
+			a.getIntent().removeExtra("ratingcount");
+			a.getIntent().putExtra("ratingcount", String.valueOf(num));
 		
-		a.getIntent().putExtra("newReview", data.getStringArrayExtra("review"));
-		a.recreate();
+			a.getIntent().putExtra("newReview", data.getStringArrayExtra("review"));
+			a.recreate();
+		}
 	}
 	
 	
